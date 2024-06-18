e60545c653c11b1b80ce6dfbf4b642339ccffeaa
Hashit <raizadaharshit2004@gmail.com>
2024-06-18 13:08:06 +0530

    ok

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -2,6 +2,14 @@ export function VideoCard() {
   return (
     <div>
       <img src="/thumbnail0.jpg"></img>
+      <div className="grid grid-cols-12">
+        <div className="col-span-1">
+          <img src="/channelimage0.jpg" className={"rounded-full w-20 h-20"} />
+        </div>
+        <div className="col-span-11 ">
+          BHAI HAI - (Official Music Video) | Fotty Seven | Bali | Enzo
+        </div>
+      </div>
     </div>
   );
 }
